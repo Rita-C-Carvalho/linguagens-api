53e10e7e0d897d5780d76c42c479164fa069dd90
Rita Carvalho <102873245+Rita-C-Carvalho@users.noreply.github.com>
2022-07-23 22:18:01 -0300

    aula 4 Imersão Java

--- src/main/java/br/com/alura/linguagens/api/Linguagem.java
+++ src/main/java/br/com/alura/linguagens/api/Linguagem.java
@@ -1,18 +1,31 @@
 package br.com.alura.linguagens.api;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "prncipaisLinguagens")
 public class Linguagem {
 
+    @Id
+    private String id;
     private String title;
     private String image;
     private int ranking;
 
-    
+    public Linguagem(){
+
+    }
 
     public Linguagem(String title, String image, int ranking) {
         this.title = title;
         this.image = image;
         this.ranking = ranking;
     }
+
+    public String getID(){
+        return id;
+    }
+
     public String getTitle() {
         return title;
     }
